873849d4501acc75ccf70323622e5735e7b54b53
org-raghav <org.raghav@gmail.com>
2021-09-09 10:04:50 +0530

    team-backend-feature01--- Java backend project initialized 1st commit

---
+++ social-app-backend/src/main/java/org/social/app/SocialAppBackendApplication.java
@@ -0,0 +1,13 @@
+package org.social.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SocialAppBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SocialAppBackendApplication.class, args);
+	}
+
+}
