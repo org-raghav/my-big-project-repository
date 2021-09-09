5be249145d3d21b1fa2572aed4160ffcd698c59e
org-raghav <org.raghav@gmail.com>
2021-09-09 10:21:57 +0530

    team-ui-feature01--- Social app updated Browser Router support addd

--- social-app-ui/src/App.js
+++ social-app-ui/src/App.js
@@ -1,24 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+
+import "./App.css";
+import About from "./components/About";
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+import HomeGuest from "./components/HomeGuest";
+import Terms from "./components/Terms";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Header />
+      <Switch>
+        <Route path="/" exact>
+          <HomeGuest />
+        </Route>
+        <Route path="/about-us">
+          <About />
+        </Route>
+        <Route path="/terms">
+          <Terms />
+        </Route>
+      </Switch>
+      <Footer />
+    </BrowserRouter>
   );
 }
 
