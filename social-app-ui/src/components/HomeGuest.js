cc10b342536e8ac2b154f25bfa48215c213bdd03
org-raghav <org.raghav@gmail.com>
2021-09-10 11:44:42 +0530

    team-ui-feature01---application Login Form updated

--- social-app-ui/src/components/HomeGuest.js
+++ social-app-ui/src/components/HomeGuest.js
@@ -27,7 +27,7 @@ function HomeGuest() {
     <Page title="Guest">
       <div className="row align-items-center">
         <div className="col-lg-7 py-3 py-md-5">
-          <h1 className="display-3">Remember Writing...</h1>
+          <h1 className="display-3">Remember Writing</h1>
           <p className="lead text-muted">
             Are you sick of short tweets and impersonal &ldquo;shared&rdquo;
             posts that are reminiscent of the late 90&rsquo;s email forwards? We
